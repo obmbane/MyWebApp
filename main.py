bcdad342604136c3b4d9970c2c39bc05fc1d9adc
obmbane <obmbaneaws@gmail.com>
2024-11-29 01:14:23 +0200

    Improved UI (Content Displayed Better)

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ Below you can find some apps that I built in python. Feel free to Reach out to m
 '''
 st.write(content2)
 
-col3,col4 =st.columns(2)
+col3, empty_col, col4 =st.columns([1.5, 0.5, 1.5])
+
 df = pd.read_csv('data.csv',sep=';')
 print(df)
 
@@ -34,7 +35,7 @@ with col3:
 
     for index, row in df[::2].iterrows():
 
-        st.header((row['title']))
+        st.subheader((row['title']))
         st.write(row['description'])
         st.image(f'web-images/{row['image']}')
         st.write(f"[source code]({row['url']})")
@@ -44,7 +45,7 @@ with col4:
 
     for index, row in df[1::2].iterrows():
 
-        st.header((row['title']))
+        st.subheader((row['title']))
         st.write(row['description'])
         st.image(f'web-images/{row['image']}')
         st.write(f"[source code]({row['url']})")
